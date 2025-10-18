9f78dc6c0fca16b7ea87b5040bc68a9dceeb01cf
Toby Davis <pencilcaseman@gmail.com>
2025-10-19 00:33:52 +0100

    feat(env): Nix and direnv
    Env updates

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "zpack developent enviornment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+
+            rustc
+            rustup
+            cargo
+
+            clang
+            llvm
+            llvmPackages.bintools
+            clang-tools
+            lldb
+          ];
+
+          buildInputs = with pkgs; [
+            pkg-config
+            zlib
+            openssl
+          ];
+        };
+      }
+    );
+}
+
